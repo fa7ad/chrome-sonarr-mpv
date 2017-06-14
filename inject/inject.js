ba994f6cba9aa3c63689c6d65933526e83b3603f
Fahad Hossain <evilinfinite7@gmail.com>
2017-06-15 03:43:37 +0600

    filter and apply
    filter and apply triggers instead of checking last or first

--- inject/inject.js
+++ inject/inject.js
@@ -1,54 +1,50 @@
 const $ = se => document.querySelector(se)
 const $$ = se => document.querySelectorAll(se)
 
+let lookForTargets
+
 if (window.sonarr) {
-  checkfortriggers()
-  setInterval(checkfortriggers, 1900)
+  addLaunchTriggers()
+  lookForTargets = setInterval(addLaunchTriggers, 1000)
 }
 
 if (!window.sonarr) {
-  let readyStateCheckInterval = setInterval(function () {
+  let checkPageState = setInterval(function () {
     if (document.readyState === 'complete') {
-      clearInterval(readyStateCheckInterval)
+      clearInterval(checkPageState)
       window.sonarr = true
-      console.log('Adding Triggers')
 
-      addtriggers()
-      setInterval(checkfortriggers, 1900)
+      console.log('[sonarr-mpv]', 'Modifying page...')
+      lookForTargets = setInterval(addLaunchTriggers, 1000)
     }
   }, 10)
 }
 
-function checkfortriggers () {
-  if ($$('.fc-event.success').length > 1 && !Array.from($$('.fc-event.success')).slice(-1)[0].getAttribute('data-sonarr')) {
-    console.log('Re-adding triggers')
-    addtriggers()
-  }
+function addLaunchTriggers () {
+  if ($$('.fc-event.success, .episode-title-cell.renderable').length > 1) {
+    const targetElements = Array.from(
+      $$('.fc-event.success, .episode-title-cell.renderable')
+    ).filter(el => !el.getAttribute('data-sonarr'))
 
-  if ($$('.episode-title-cell.renderable').length > 1 && !Array.from($$('.episode-title-cell.renderable')).slice(-1)[0].getAttribute('data-sonarr')) {
-    console.log('Re-adding triggers')
-    addtriggers()
+    targetElements.forEach(t => {
+      t.addEventListener('click', () => setTimeout(addMpvLink, 1100))
+      t.setAttribute('data-sonarr', 'added')
+    })
+  } else {
+    console.warn('[sonarr-mpv]', "This page doesn't have any supported target.")
+    clearInterval(lookForTargets)
+    lookForTargets = setInterval(addLaunchTriggers, 5000)
   }
 }
 
-function turntolink () {
-  const ln = $('div.episode-file-info tbody .renderable.string-cell')
-  if (ln) {
-    ln.style.color = 'rgb(0, 170, 250)'
-    ln.style.cursor = 'pointer'
-    ln.addEventListener('click', function (e) {
+function addMpvLink () {
+  const popup = $('div.episode-file-info tbody .renderable.string-cell')
+  if (popup) {
+    popup.style.color = 'rgb(0, 170, 250)'
+    popup.style.cursor = 'pointer'
+    popup.addEventListener('click', e => {
       e.preventDefault()
-      window.location.replace(`mpv://${ln.innerText.trim()}`)
+      window.location.replace(`mpv://${popup.innerText.trim()}`)
     })
   }
-}
-
-function addtriggers () {
-  function addClickEvt (El) {
-    El.addEventListener('click', () => setTimeout(turntolink, 1000), false)
-    El.setAttribute('data-sonarr', 'added')
-  }
-
-  $$('.fc-event.success').forEach(addClickEvt)
-  $$('.episode-title-cell.renderable').forEach(addClickEvt)
 }
