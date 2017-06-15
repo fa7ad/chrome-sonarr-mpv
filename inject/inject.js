e5d71290462a35bc60e180e87a552363ab77f7bc
Fahad Hossain <evilinfinite7@gmail.com>
2017-06-15 23:21:16 +0600

    add some transition and clean up code slightly

--- inject/inject.js
+++ inject/inject.js
@@ -3,11 +3,6 @@ const $$ = se => document.querySelectorAll(se)
 
 let lookForTargets
 
-if (window.sonarr) {
-  addLaunchTriggers()
-  lookForTargets = setInterval(addLaunchTriggers, 1000)
-}
-
 if (!window.sonarr) {
   let checkPageState = setInterval(function () {
     if (document.readyState === 'complete') {
@@ -15,36 +10,54 @@ if (!window.sonarr) {
       window.sonarr = true
 
       console.log('[sonarr-mpv]', 'Modifying page...')
-      lookForTargets = setInterval(addLaunchTriggers, 1000)
     }
   }, 10)
 }
 
-function addLaunchTriggers () {
-  if ($$('.fc-event.success, .episode-title-cell.renderable').length > 1) {
-    const targetElements = Array.from(
-      $$('.fc-event.success, .episode-title-cell.renderable')
-    ).filter(el => !el.getAttribute('data-sonarr'))
+if (lookForTargets) clearInterval(lookForTargets)
+lookForTargets = setInterval(addLaunchTriggers, 1000)
 
-    targetElements.forEach(t => {
-      t.addEventListener('click', () => setTimeout(addMpvLink, 1100))
-      t.setAttribute('data-sonarr', 'added')
-    })
+function addLaunchTriggers () {
+  const targets = $$('.fc-event.success, .episode-title-cell.renderable')
+
+  if (targets.length > 1) {
+    Array.from(targets)
+      .filter(t => !t.getAttribute('data-sonarr'))
+      .forEach(t => {
+        t.addEventListener('click', () => setTimeout(addMpvLink, 1000))
+        t.setAttribute('data-sonarr', 'added')
+      })
   } else {
-    console.warn('[sonarr-mpv]', "This page doesn't have any supported target.")
-    clearInterval(lookForTargets)
-    lookForTargets = setInterval(addLaunchTriggers, 5000)
+    // Popup might already be on screen
+    addMpvLink()
+
+    // Or not
+    console.warn('[sonarr-mpv]', 'No supported targets on this page.')
+
+    // Increase the interval -- waiting for page change
+    if (lookForTargets) clearInterval(lookForTargets)
+    lookForTargets = setInterval(addLaunchTriggers, 4816)
   }
 }
 
 function addMpvLink () {
-  const popup = $('div.episode-file-info tbody .renderable.string-cell')
-  if (popup) {
-    popup.style.color = 'rgb(0, 170, 250)'
-    popup.style.cursor = 'pointer'
-    popup.addEventListener('click', e => {
+  const fileLink = $('div.episode-file-info tbody .renderable.string-cell')
+  if (fileLink) {
+    // Make it look like a link
+    Object.assign(fileLink.style, {
+      color: 'rgb(0, 170, 250)',
+      cursor: 'pointer',
+      transition: 'all ease-in 500ms'
+    })
+
+    // Open mpv on click
+    fileLink.addEventListener('click', function (e) {
       e.preventDefault()
-      window.location.replace(`mpv://${popup.innerText.trim()}`)
+      this.style.color = 'purple'
+      setTimeout(() => {
+        this.style.color = 'rgb(0, 170, 250)'
+      }, 5000)
+      window.location.replace(`mpv://${fileLink.innerText.trim()}`)
     })
   }
 }
