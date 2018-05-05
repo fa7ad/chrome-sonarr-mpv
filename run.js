a4eb8b779727bc9eb294eab6374b89c11aa63b28
Fahad Hossain <evilinfinite7@gmail.com>
2018-05-05 13:40:33 +0600

    use jquery more efficiently

--- run.js
+++ run.js
@@ -1,24 +1,21 @@
-const jq2 = window.jQuery.noConflict()
-jq2(function ($) {
-  let findTargets
+window.jQuery.noConflict()(function ($) {
   console.info('[sonarr-mpv]', 'Modifying page...')
 
-  if (findTargets) clearInterval(findTargets)
-  findTargets = setInterval(modTargets, 1000)
+  let findTargets
+  findTargets = reset(modTargets, 1, findTargets)
 
   function modTargets () {
-    const els = $('.fc-event.success, .episode-title-cell.renderable')
+    let noEl = true
+    $('.fc-event.success, .episode-title-cell.renderable').each(function (id) {
+      $(this).off('click').on('click', e => setTimeout(addMpvLink, 1e3))
+      noEl && (noEl = false)
+    })
 
-    addMpvLink()
-    if (els.length > 1) {
-      return els.each(function () {
-        $(this).on('click', e => setTimeout(addMpvLink, 1000))
-      })
+    if (noEl) {
+      addMpvLink()
+      console.warn('[sonarr-mpv]', 'No supported targets on this page.')
+      findTargets = reset(modTargets, 4.5, findTargets)
     }
-
-    console.warn('[sonarr-mpv]', 'No supported targets on this page.')
-    if (findTargets) clearInterval(findTargets)
-    findTargets = setInterval(modTargets, 4816)
   }
 
   function addMpvLink () {
@@ -26,11 +23,18 @@ jq2(function ($) {
       .css('color', '#00AAFA')
       .css('cursor', 'pointer')
       .css('transition', 'all ease-in 900ms')
+      .off('click')
       .on('click', function (e) {
         e.preventDefault()
         const self = $(this)
         self.css('color', 'purple')
+        setTimeout(() => self.css('color', '#00AAFA'), 3e3)
         window.location.replace(`mpv://${self.text().trim()}`)
       })
   }
 })
+
+function reset (cb, interval, timer) {
+  if (timer) clearInterval(timer)
+  return setInterval(cb, interval*1e3)
+}
